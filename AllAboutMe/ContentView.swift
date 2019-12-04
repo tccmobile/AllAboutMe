0bd86f7422bc8cb6b7dbb9c18b4ed1ad85a0f27d
Will Smith <drwillsmith@gmail.com>
2019-12-04 14:00:02 -0600

    Initial Commit

---
+++ AllAboutMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AllAboutMe
+//
+//  Created by William Smith on 12/4/19.
+//  Copyright © 2019 William Smith. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
